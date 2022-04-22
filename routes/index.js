d0e23dba769c5c8b0d29e0f819c1782e1d124003
iballinger <ilerdonballinger@gmail.com>
2022-04-22 10:27:28 -0700

    Connect to database.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Servant Tracker' });
 });
 
 module.exports = router;
