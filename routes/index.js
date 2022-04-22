5683ea55f50105800c378503b0bc107731447782
iballinger <ilerdonballinger@gmail.com>
2022-04-22 11:14:12 -0700

    Landing splash.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const passport = require('passport');
 
 router.get('/', function(req, res, next) {
-  res.redirect('/servants');
+  res.render('', {title: 'Welcome'});
 });
 
 router.get('/auth/google', passport.authenticate(
