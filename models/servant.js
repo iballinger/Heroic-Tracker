6a6070b98f5c2484e7fa09ff1b5e419db279db82
iballinger <ilerdonballinger@gmail.com>
2022-04-22 16:32:54 -0700

    Manual Servant add functionality.

--- models/servant.js
+++ models/servant.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const servantSchema = new Schema ({
+    id: Number,
+    collectionNo: Number,
     name: String,
-    class: String,
+    flag: String,
+    className: String,
+    attribute: String,
+    rarity: Number,
+    atkMax: Number,
+    hpMax: Number,
+    face: String,
 });
 
 module.exports = mongoose.model('Servant', servantSchema);
---
+++ seed.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+require('./config/database');
+
+const Servant = require('./models/servant');
+
+const data = require('./data');
+
+const p1 = Servant.deleteMany({});
+
+Promise.all([p1])
+    .then(function() {
+        return Servant.create(data);
+    })
